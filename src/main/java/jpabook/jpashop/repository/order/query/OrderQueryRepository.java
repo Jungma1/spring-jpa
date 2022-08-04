56834e65d8ae61d6a692f780eb6b4a984f692722
Jungma1 <mbjun3630@naver.com>
2022-08-04 20:58:31 +0900

    주문 조회 V5: 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.repository.order.query;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -20,6 +22,34 @@ public class OrderQueryRepository {
         });
 
         return result;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        // IN 쿼리로 한 번에 조회 -> 메모리에 저장 -> result 에 값 입력 -> 실질적인 쿼리는 2번 (N + 1 문제 해결)
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count) from OrderItem oi"
+                                + " join oi.item i"
+                                + " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        return orderItems.stream()
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                .map(OrderQueryDto::getOrderId)
+                .collect(Collectors.toList());
     }
 
     private List<OrderQueryDto> findOrders() {
